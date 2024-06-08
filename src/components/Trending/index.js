8300cead773e29721b2fc4d8254ba2236287d7ff
pratik <pratikkrverma@gmail.com>
2024-06-08 13:37:25 +0000

    [001] Completed Trending section

--- src/components/Trending/index.js
+++ src/components/Trending/index.js
@@ -1,19 +1,98 @@
 import {Component} from 'react'
+import Cookies from 'js-cookie'
+import {Link} from 'react-router-dom'
+import {AiFillFire} from 'react-icons/ai'
 import {
   TrendingContainer,
   TopSection,
+  FireContainer,
   CustomHeading,
+  TrendingCardContainer,
   TrendingCard,
+  TrendingCardImg,
+  TrendingCardDetails,
+  TrendingCardTitle,
+  TrendingCardViewTime,
+  TrendingCardName,
+  View,
+  Name,
+  Time,
+  StyledBsDot,
 } from './styledComponents'
 
 class Trending extends Component {
+  state = {trendingData: []}
+
+  componentDidMount() {
+    this.getTrendingVideo()
+  }
+
+  getTrendingVideo = async () => {
+    const jwtToken = Cookies.get('jwt_token')
+
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+
+    const response = await fetch(
+      'https://apis.ccbp.in/videos/trending',
+      options,
+    )
+    const responseObj = await response.json()
+    const trendingData = responseObj.videos.map(vid => ({
+      id: vid.id,
+      publishedAt: vid.published_at,
+      thumbnailUrl: vid.thumbnail_url,
+      title: vid.title,
+      viewCount: vid.view_count,
+      channel: {
+        name: vid.channel.name,
+        profileImageUrl: vid.channel.profile_image_url,
+      },
+    }))
+    this.setState({
+      trendingData,
+    })
+    console.log(trendingData)
+  }
+
   render() {
+    const {trendingData} = this.state
+    console.log(trendingData)
+    if (!trendingData) {
+      return <p>Loading...</p>
+    }
     return (
       <TrendingContainer>
         <TopSection>
+          <FireContainer>
+            <AiFillFire size={35} style={{color: 'red'}} />
+          </FireContainer>
           <CustomHeading>Trending</CustomHeading>
         </TopSection>
-        <TrendingCard />
+        <TrendingCardContainer>
+          {trendingData.map(vid => (
+            <TrendingCard>
+              <Link to={`/video/${vid.id}`}>
+                <TrendingCardImg src={vid.thumbnailUrl} alt="" />
+              </Link>
+              <Link to={`/video/${vid.id}`}>
+                <TrendingCardDetails>
+                  <TrendingCardTitle>{vid.title}</TrendingCardTitle>
+                  <TrendingCardName>{vid.channel.name}</TrendingCardName>
+                  <TrendingCardViewTime>
+                    <View>{`${vid.viewCount} views`}</View>
+                    <StyledBsDot />
+                    <Time>{vid.publishedAt}</Time>
+                  </TrendingCardViewTime>
+                </TrendingCardDetails>
+              </Link>
+            </TrendingCard>
+          ))}
+        </TrendingCardContainer>
       </TrendingContainer>
     )
   }
--- src/components/Trending/styledComponents.js
+++ src/components/Trending/styledComponents.js
@@ -1,6 +1,44 @@
 import styled from 'styled-components'
+import {BsDot} from 'react-icons/bs'
 
 export const TrendingContainer = styled.div``
-export const TopSection = styled.div``
-export const CustomHeading = styled.div``
-export const TrendingCard = styled.div``
+export const TopSection = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #ebebeb;
+  padding: 20px 40px;
+`
+export const FireContainer = styled.div`
+  border: 1px red solid;
+  border-radius: 100%;
+  margin-right: 20px;
+  padding: 16px;
+`
+export const CustomHeading = styled.h1``
+export const TrendingCardContainer = styled.ul`
+  padding: 40px;
+  padding-top: 32px;
+`
+export const TrendingCard = styled.li`
+  display: flex;
+  margin-bottom: 40px;
+`
+
+export const TrendingCardImg = styled.img`
+  width: 250px;
+  margin-right: 20px;
+`
+export const TrendingCardDetails = styled.div``
+export const TrendingCardTitle = styled.div``
+export const TrendingCardViewTime = styled.div`
+  display: flex;
+  align-items: center;
+`
+export const TrendingCardName = styled.div``
+export const View = styled.p``
+export const Name = styled.p``
+export const Time = styled.p``
+export const StyledBsDot = styled(BsDot)`
+  font-size: 18px;
+  margin-top: 4px;
+`
