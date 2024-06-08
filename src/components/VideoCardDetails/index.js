450cf4bd57cd5fef686fe9f60e0377ef57e296b0
pratik <pratikkrverma@gmail.com>
2024-06-08 09:59:36 +0000

    [001] Added Video details page partially

--- src/components/VideoCardDetails/index.js
+++ src/components/VideoCardDetails/index.js
@@ -1,12 +1,29 @@
 import {Component} from 'react'
 import Cookies from 'js-cookie'
+import ReactPlayer from 'react-player'
+import {AiOutlineLike, AiOutlineDislike} from 'react-icons/ai'
 import Header from '../Header'
 import SideBar from '../SideBar'
 import {
   VideoCardDetailsContainer,
   VideoCardDetailsDescriptionContainer,
   VideoCardDetailsDescriptionVideo,
-  VideoCardDetailsDescriptionVideoSrc,
+  VideoCardDetailsDescriptionPara,
+  VideoCardDetailsDescriptionDetailsContainerTop,
+  Views,
+  Time,
+  VideoCardDetailsDescriptionDetailsContainerBottom,
+  VideoCardDetailsDescriptionDetailsContainer,
+  LikeContainer,
+  LikePara,
+  ChannelDetails,
+  CustomChannelImg,
+  TitleChannelContainer,
+  ChannelSubDetails,
+  ChannelName,
+  Subscribers,
+  Title,
+  StyledBsDot,
 } from './styledComponents'
 
 class VideoCardDetails extends Component {
@@ -51,17 +68,63 @@ class VideoCardDetails extends Component {
 
   render() {
     const {videoData} = this.state
-    console.log(videoData.videoUrl)
+    if (!videoData.channel) {
+      return <p>Loading...</p>
+    }
+    console.log(videoData.channel.profileImageUrl)
+
     return (
       <>
         <Header />
         <VideoCardDetailsContainer>
           <SideBar />
           <VideoCardDetailsDescriptionContainer>
-            <VideoCardDetailsDescriptionVideo controls>
-              <source src={videoData.videoUrl} type="video/mp4" />
+            <VideoCardDetailsDescriptionVideo>
+              <ReactPlayer
+                url={videoData.videoUrl}
+                controls
+                width="100%"
+                height="500px"
+              />
             </VideoCardDetailsDescriptionVideo>
-            {videoData.description}
+            <VideoCardDetailsDescriptionPara>
+              {videoData.title}
+            </VideoCardDetailsDescriptionPara>
+            <VideoCardDetailsDescriptionDetailsContainer>
+              <VideoCardDetailsDescriptionDetailsContainerTop>
+                <Views>{`${videoData.viewCount} views`}</Views>
+                <StyledBsDot />
+                <Time>{videoData.publishedAt}</Time>
+              </VideoCardDetailsDescriptionDetailsContainerTop>
+              <VideoCardDetailsDescriptionDetailsContainerBottom>
+                <LikeContainer>
+                  <AiOutlineLike size={24} />
+                  <LikePara>Like</LikePara>
+                </LikeContainer>
+                <LikeContainer>
+                  <AiOutlineDislike size={24} />
+                  <LikePara>Dislike</LikePara>
+                </LikeContainer>
+                <LikeContainer>
+                  <AiOutlineDislike size={24} />
+                  <LikePara>Save</LikePara>
+                </LikeContainer>
+              </VideoCardDetailsDescriptionDetailsContainerBottom>
+            </VideoCardDetailsDescriptionDetailsContainer>
+            <hr />
+            <ChannelDetails>
+              <CustomChannelImg
+                src={videoData.channel.profileImageUrl}
+                alt=""
+              />
+              <TitleChannelContainer>
+                <ChannelSubDetails>
+                  <ChannelName>{videoData.channel.name}</ChannelName>
+                  <Subscribers>{`${videoData.channel.subscriberCount} subscribers`}</Subscribers>
+                </ChannelSubDetails>
+                <Title>{videoData.title}</Title>
+              </TitleChannelContainer>
+            </ChannelDetails>
           </VideoCardDetailsDescriptionContainer>
         </VideoCardDetailsContainer>
       </>
--- src/components/VideoCardDetails/styledComponents.js
+++ src/components/VideoCardDetails/styledComponents.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import {BsDot} from 'react-icons/bs'
 
 export const VideoCardDetailsContainer = styled.div`
   display: flex;
@@ -8,10 +9,64 @@ export const VideoCardDetailsContainer = styled.div`
   min-height: 100vh;
   width: 100%;
 `
-export const VideoCardDetailsDescriptionContainer = styled.div``
-export const VideoCardDetailsDescriptionVideo = styled.video`
+export const VideoCardDetailsDescriptionContainer = styled.div`
+  width: 100%;
+  padding: 28px;
+`
+export const VideoCardDetailsDescriptionVideo = styled.div`
   width: 100%;
   height: auto;
   margin-bottom: 20px;
 `
-export const VideoCardDetailsDescriptionVideoSrc = styled.source``
+
+export const VideoCardDetailsDescriptionPara = styled.p``
+export const VideoCardDetailsDescriptionDetailsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+export const VideoCardDetailsDescriptionDetailsContainerTop = styled.div`
+  display: flex;
+  align-items: center;
+`
+export const Views = styled.p``
+export const Time = styled.p``
+export const VideoCardDetailsDescriptionDetailsContainerBottom = styled.div`
+  display: flex;
+  align-items: center;
+`
+export const LikeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 12px;
+  margin-left: 12px;
+`
+export const LikePara = styled.p`
+  margin-right: 4px;
+  margin-left: 4px;
+`
+
+export const ChannelDetails = styled.div`
+  display: flex;
+  align-items: flex-start;
+`
+export const CustomChannelImg = styled.img`
+  width: 60px;
+  margin-right: 30px;
+`
+export const TitleChannelContainer = styled.div``
+export const ChannelSubDetails = styled.div``
+export const ChannelName = styled.p`
+  margin-bottom: 8px;
+  margin-top: 2px;
+`
+export const Subscribers = styled.p`
+  margin-top: 8px;
+  margin-bottom: 24px;
+`
+export const Title = styled.p``
+
+export const StyledBsDot = styled(BsDot)`
+  font-size: 18px;
+  margin-top: 4px;
+`
